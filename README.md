385b6c930bca92550c79cac68ed70fa2863cc56b
Kris Surette <104463248+VTskier@users.noreply.github.com>
2025-03-05 17:45:25 -0500

    initial commit -- working interview, cleaned up a bit

---
+++ README.md
@@ -0,0 +1,8 @@
+# docassemble.FinancialDisclosureForm100001
+
+Financial Disclosure - form 100-00127
+
+## Author
+
+K. Surette, Legal Services Vermont
+
---
+++ setup.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from setuptools import setup, find_namespace_packages
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+
+standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+
+def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
+    out = {}
+    stack = [(convert_path(where), '', package)]
+    while stack:
+        where, prefix, package = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                        stack.append((fn, '', new_package))
+                else:
+                    stack.append((fn, prefix + name + '/', package))
+            else:
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+setup(name='docassemble.VTFinancialDisclosure',
+      version='1.0',
+      description=('Financial Disclosure - form 100-00127'),
+      long_description='# docassemble.FinancialDisclosureForm100001\r\n\r\nFinancial Disclosure - form 100-00127\r\n\r\n## Author\r\n\r\nK. Surette, Legal Services Vermont\r\n\r\n',
+      long_description_content_type='text/markdown',
+      author='K. Surette, Legal Services Vermont',
+      author_email='ksurette@legalservicesvt.org',
+      license='The MIT License',
+      url='https://VTCourtForms.org',
+      packages=find_namespace_packages(),
+      install_requires=['docassemble.ALToolbox>=0.11.1', 'docassemble.AssemblyLine>=3.2.0', 'docassemble.GithubFeedbackForm>=0.4.1.1', 'docassemble.VTCourtFormsBranding', 'docassemble.VTFeedback', 'docassemble.VTSharedYMLFile'],
+      zip_safe=False,
+      package_data=find_package_data(where='docassemble/VTFinancialDisclosure/', package='docassemble.VTFinancialDisclosure'),
+     )
+
